0969d7891213b02ada0a85dd8db694fdfe572aea
utgwkk <utagawakiki@gmail.com>
2020-10-03 08:56:33 +0000

    Revert "15"
    This reverts commit fe02afeb18581e7b4cd67178086724b534badf51.

--- golang/cmd/xsuportal/main.go
+++ golang/cmd/xsuportal/main.go
@@ -41,7 +41,7 @@ import (
 )
 
 const (
-	TeamCapacity               = 15
+	TeamCapacity               = 10
 	AdminID                    = "admin"
 	AdminPassword              = "admin"
 	DebugContestStatusFilePath = "/tmp/XSUPORTAL_CONTEST_STATUS"
